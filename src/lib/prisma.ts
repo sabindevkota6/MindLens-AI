88853417bd9386ac67ab8dc78c4e685557750da5
Sabin Devkota <sabindevkota6@gmail.com>
2026-04-20 22:00:51 +0545

    Updated Query logging to only activate in development and stay silent in production.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,7 +5,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["query"], // logs sql queries
+    log: process.env.NODE_ENV === "development" ? ["query"] : [],
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
